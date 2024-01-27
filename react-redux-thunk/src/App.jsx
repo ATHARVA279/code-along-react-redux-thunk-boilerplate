aff4c16ca06000436920a362d33bdbf9f29f2fbc
ATHARVA279 <141947324+ATHARVA279@users.noreply.github.com>
2024-01-27 16:30:25 +0530

    added

---
+++ react-redux-thunk/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DisplayData from './Redux/DisplayData'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <DisplayData/>
+    </div>
+  )
+}
+
+export default App
